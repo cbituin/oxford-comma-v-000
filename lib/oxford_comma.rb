d8b258009ebe8973de4ce3c6e6f35cc5e857d0bd
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-09-11 19:17:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,13 +5,7 @@ def oxford_comma(array)
     array.join(" and ")
 
   elsif array.size >= 3
-    # newArray = []; counter = 0
-    # until counter === array.size - 1
-    #   newArray << array[counter]
-    #   counter+=1 
-    # end
-    # newArray.push("and")
-    # newArray.push(array[array.size-1])
+    array[-1] = "and " + array[-1]
     array.join(", ")
   end
 
